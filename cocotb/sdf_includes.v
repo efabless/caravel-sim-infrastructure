0d7f573b16359fa037597885a285beb2216029b5
M0stafaRady <mostafa.rady@efabless.com>
2023-01-15 01:40:54 -0800

    change caravel to swift caravel for swift design

--- cocotb/sdf_includes.v
+++ cocotb/sdf_includes.v
@@ -1,6 +1,10 @@
 initial begin
     `ifndef CARAVAN
+    `ifdef ARM
+    $sdf_annotate({`CARAVEL_ROOT,"/signoff/swift_caravel/primetime/sdf/",`SDF_POSTFIX,"/swift_caravel.",`CORNER ,".sdf"}, uut,,{`MAIN_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/caravel_sdf.log"},`ifdef MAX_SDF "MAXIMUM" `else "MINIMUM" `endif ); 
+    `else
     $sdf_annotate({`CARAVEL_ROOT,"/signoff/caravel/primetime/sdf/",`SDF_POSTFIX,"/caravel.",`CORNER ,".sdf"}, uut,,{`MAIN_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/caravel_sdf.log"},`ifdef MAX_SDF "MAXIMUM" `else "MINIMUM" `endif ); 
+    `endif //ARM
     `else
     $sdf_annotate({`CARAVEL_ROOT,"/signoff/caravan/primetime/sdf/",`SDF_POSTFIX,"/caravan.", `CORNER,".sdf"}, uut,,{`MAIN_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/caravan_sdf.log"},`ifdef MAX_SDF "MAXIMUM" `else "MINIMUM" `endif ); 
     `endif
