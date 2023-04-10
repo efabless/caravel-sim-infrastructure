302858a7332ce591928f05a019d16f7c27a83670
M0stafaRady <mostafa.rady@efabless.com>
2023-04-10 04:28:35 -0700

    changes for adding user project sdf files

--- cocotb/sdf_includes.v
+++ cocotb/sdf_includes.v
@@ -5,7 +5,11 @@ initial begin
     `else
     $sdf_annotate({`CARAVEL_ROOT,"/signoff/caravel/primetime/sdf/",`SDF_POSTFIX,"/caravel.",`CORNER ,".sdf"}, uut,,{`COCOTB_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/caravel_sdf.log"},`ifdef MAX_SDF "MAXIMUM" `else "MINIMUM" `endif ); 
     `endif //ARM
-    `else
+    `else // CARAVAN
     $sdf_annotate({`CARAVEL_ROOT,"/signoff/caravan/primetime/sdf/",`SDF_POSTFIX,"/caravan.", `CORNER,".sdf"}, uut,,{`COCOTB_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/caravan_sdf.log"},`ifdef MAX_SDF "MAXIMUM" `else "MINIMUM" `endif ); 
     `endif
+
+    // `ifdef USER_SDF_ENABLE
+    $sdf_annotate({`USER_PROJECT_ROOT,"/signoff/user_project_wrapper/primetime/sdf/",`SDF_POSTFIX,"/user_project_wrapper.", `CORNER,".sdf"}, uut.chip_core.mprj,,{`COCOTB_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/user_prog_sdf.log"},`ifdef MAX_SDF "MAXIMUM" `else "MINIMUM" `endif ); 
+    // `endif // USER_SDF_ENABLE
 end
