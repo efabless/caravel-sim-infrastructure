43015e754c2dea9ff1495dca33d5d1502e6322ea
M0stafaRady <mostafa.rady@efabless.com>
2023-06-07 01:46:13 -0700

    update irq_apis to clear the interrupt when disabled

--- cocotb/interfaces/common_functions/irq_api.h
+++ cocotb/interfaces/common_functions/irq_api.h
@@ -61,8 +61,9 @@ void enable_external1_irq(bool is_enable){
         #ifndef ARM
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << USER_IRQ_4_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << USER_IRQ_4_INTERRUPT));
         reg_user4_irq_en =0;
+        user_irq_4_ev_pending_i0_write(1); // to clear the interrupt
         #else
         NVIC_DisableIRQ(HK_IRQ1);
         __enable_irq();
@@ -91,8 +92,9 @@ void enable_external2_irq(bool is_enable){
         #ifndef ARM
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << USER_IRQ_5_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << USER_IRQ_5_INTERRUPT));
         reg_user5_irq_en =0;
+        user_irq_5_ev_pending_i0_write(1); // to clear the interrupt
         #else
         NVIC_DisableIRQ(HK_IRQ2);
         __enable_irq();
@@ -121,9 +123,10 @@ void enable_user0_irq(bool is_enable){
         #ifndef ARM
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << USER_IRQ_0_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(0 << USER_IRQ_0_INTERRUPT));
         reg_user0_irq_en =0;
         reg_user_irq_enable &=0x6;
+        user_irq_0_ev_pending_i0_write(1); // to clear the interrupt
         #else
         NVIC_DisableIRQ(HK_IRQ2);
         __enable_irq();
@@ -152,9 +155,10 @@ void enable_user1_irq(bool is_enable){
         #ifndef ARM
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << USER_IRQ_1_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << USER_IRQ_1_INTERRUPT));
         reg_user1_irq_en =0;
         reg_user_irq_enable &=0x5;
+        user_irq_1_ev_pending_i0_write(1); // to clear the interrupt
         #else
         NVIC_DisableIRQ(HK_IRQ2);
         __enable_irq();
@@ -184,9 +188,10 @@ void enable_user2_irq(bool is_enable){
         #ifndef ARM
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << USER_IRQ_2_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << USER_IRQ_2_INTERRUPT));
         reg_user2_irq_en =0;
         reg_user_irq_enable &=0x3;
+        user_irq_2_ev_pending_i0_write(1); // to clear the interrupt
         #else
         NVIC_DisableIRQ(HK_IRQ2);
         __enable_irq();
@@ -215,8 +220,9 @@ void enable_timer0_irq(bool is_enable){
         #ifndef ARM
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << TIMER0_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << TIMER0_INTERRUPT));
         reg_timer0_irq_en = 0;
+        timer0_ev_pending_zero_write(1); // to clear the interrupt
         #else
         NVIC_DisableIRQ(TMR0_IRQn);
         reg_timer0_config = reg_timer0_config | 0x8; // enable irq
@@ -248,7 +254,8 @@ void enable_uart_tx_irq(bool is_enable){
         reg_uart_irq_en =0;
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << UART_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << UART_INTERRUPT));
+        uart_ev_pending_tx_write(1);    // to clear the interrupt
         #else
         NVIC_DisableIRQ(UART0_IRQn);
         reg_uart_ctrl = reg_uart_ctrl | 0x5; // enable irq TX 
@@ -281,7 +288,8 @@ void enable_uart_rx_irq(bool is_enable){
         reg_uart_irq_en =1;
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << UART_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << UART_INTERRUPT));
+        uart_ev_pending_rx_write(1);    // to clear the interrupt
         #else
         reg_uart_ctrl = reg_uart_ctrl & 0xF7; // enable irq rx 
         __enable_irq();
@@ -309,8 +317,9 @@ void enable_hk_spi_irq(bool is_enable){
         #ifndef ARM
         irq_setmask(0);
         irq_setie(1);
-        irq_setmask(irq_getmask() | (1 << USER_IRQ_3_INTERRUPT));
+        irq_setmask(irq_getmask() & ~(1 << USER_IRQ_3_INTERRUPT));
         reg_user3_irq_en =0;
+        user_irq_3_ev_pending_i0_write(1); // to clear the interrupt
         #else
         NVIC_DisableIRQ(HK_IRQ0);
         __enable_irq();
